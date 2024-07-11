d0574f8bacd4c2b139b5ff44c8199c5e080b8757
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2024-07-11 17:16:47 +0200

    Added validation for Account Update in AccountList. Next step is to duplicate the code to the other pages with the same logic

---
+++ BankApp/wwwroot/js/AccountListJava.js
@@ -0,0 +1,24 @@
+﻿//document.addEventListener('DOMContentLoaded', function () {
+//    var form = document.getElementById('Accountinformationform');
+//    var submitButton = document.getElementById('editanddeleteModal');
+
+//    form.addEventListener('input', function () {
+//        if (form.checkValidity()) {
+//            submitButton.disabled = false;
+//        } else {
+//            submitButton.disabled = true;
+//        }
+//    });
+//});
+
+//const InputAccountName = document.getElementById("InputAccountName");
+//var form = document.getElementById('Accountinformationform');
+//var submitButton = documentgetElementById('editanddeleteModal');
+
+//InputAccountName.addEventListener("keydown", () => {
+//    if (form.checkValidity()) { // Check validity of the entire form
+//        submitButton.disabled = false;
+//    } else {
+//        submitButton.disabled = true;
+//    }
+//});
